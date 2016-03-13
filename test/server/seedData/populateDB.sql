7933cdd554c5ddca9e79e7c89f147575c61e3685
Eduardo M. Breijo <eduardo.breijo@upr.edu>
2016-03-13 19:49:16 -0400

    Added authentication controllers and routes for login and logout.

--- test/server/seedData/populateDB.sql
+++ test/server/seedData/populateDB.sql
@@ -17,13 +17,13 @@ INSERT INTO administrator_access (email, is_root, admin_account_status)
 VALUES ('maria.hernandez@upr.edu', 0, 'inactive');
 
 INSERT INTO administrator (email, password, first_name, last_name)
-VALUES ('placement@uprm.edu', 'pass', 'Placement', 'Office');
+VALUES ('placement@uprm.edu', '$2a$08$IA2R7uGJON4f89wOEQLibu9TGtrCfZEIsC7qxCwYL4nuAs.feG4MC', 'Placement', 'Office');
 
 INSERT INTO administrator (email, password, first_name, last_name)
-VALUES ('juan.rodriguez@upr.edu', 'pass', 'Juan', 'Rodriguez');
+VALUES ('juan.rodriguez@upr.edu', '$2a$08$IA2R7uGJON4f89wOEQLibu9TGtrCfZEIsC7qxCwYL4nuAs.feG4MC', 'Juan', 'Rodriguez');
 
 INSERT INTO administrator (email, password, first_name, last_name)
-VALUES ('maria.hernandez@upr.edu', 'pass', 'Maria', 'Hernandez');
+VALUES ('maria.hernandez@upr.edu', '$2a$08$IA2R7uGJON4f89wOEQLibu9TGtrCfZEIsC7qxCwYL4nuAs.feG4MC', 'Maria', 'Hernandez');
 
 INSERT INTO company (name, website_url, company_description, company_status)
 VALUES ('IBM', 'http://www.ibm.com/us-en/', 'This is IBM', 'active');
@@ -92,13 +92,13 @@ INSERT INTO job_fair_dates (header_en, location_en, date_en, time, header_es, lo
 VALUES ('8th Spring Job Fair', 'Mayaguez Resort & Casino', 'Friday, February 19, 2016', '8:30am - 2:30pm', '8va Feria de Empleo de Primavera', 'Hotel Mayaguez Resort & Casino', 'viernes, 19 de febrero de 2016', '2016-02-19');
 
 INSERT INTO recruiter (email, password, company_name, first_name, last_name, phone_number, account_status, company_location)
-VALUES ('sscosta@us.ibm.com', 'pass', 'IBM', 'Sergio', 'Costa', '787-555-5555', 'active', '1');
+VALUES ('sscosta@us.ibm.com', '$2a$08$IA2R7uGJON4f89wOEQLibu9TGtrCfZEIsC7qxCwYL4nuAs.feG4MC', 'IBM', 'Sergio', 'Costa', '787-555-5555', 'active', '1');
 
 INSERT INTO recruiter (email, password, company_name, first_name, last_name, phone_number, company_location)
-VALUES ('juanito@gmail.com', 'pass', 'Google', 'Juanito', 'Perez', '787-555-5555', '2');
+VALUES ('juanito@gmail.com', '$2a$08$IA2R7uGJON4f89wOEQLibu9TGtrCfZEIsC7qxCwYL4nuAs.feG4MC', 'Google', 'Juanito', 'Perez', '787-555-5555', '2');
 
 INSERT INTO recruiter (email, password, company_name, first_name, last_name, phone_number, account_status, company_location)
-VALUES ('pancho@evertec.com', 'pass', 'EVERTEC', 'Pancho', 'Rivera', '787-555-5555', 'inactive', '3');
+VALUES ('pancho@evertec.com', '$2a$08$IA2R7uGJON4f89wOEQLibu9TGtrCfZEIsC7qxCwYL4nuAs.feG4MC', 'EVERTEC', 'Pancho', 'Rivera', '787-555-5555', 'inactive', '3');
 
 INSERT INTO job_offer (company_name, title, description, job_position, education_level, recent_graduate, expiration_date, job_offer_status, creation_date, recruiter_email, location)
 VALUES ('IBM', 'New Job Offer', 'This is a job offer', 'Full-Time', 'Bachelors', '1', '2016-07-22 12:12:12', 'approved', '2016-02-22 12:12:12', 'sscosta@us.ibm.com', 'Durham, NC');
