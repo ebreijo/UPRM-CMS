b6d1230aeefa47fad70d16eb402a635cec60c9cc
Jean <jean.mendez2@upr.edu>
2016-03-20 01:21:40 -0400

    Updated Dropzone Directive with better documentation.

--- app/scripts/directives/fileUploadDirective.js
+++ app/scripts/directives/fileUploadDirective.js
@@ -1,18 +1,16 @@
+'use strict';
+
 // FOR REFERENCE:
 // https://gist.github.com/compact/8118670#file-dropzone-directive-js-L37
 
 // TODO: Integrate this Directive with this project's structure.
-'use strict';
-
 /*
-app.directive('fileDirective', function () {
+app.directive('uprmFileUploader', function () {
   return function (scope, element, attrs) {
-
     var config, dropzone;
 
-    config = scope[attrs.dropzone];
+    config = scope[attrs.uprmFileUploader];
 
-    // TODO: Add Dropzone import in app.js.
     // create a Dropzone for the element with the given options
     dropzone = new Dropzone(element[0], config.options);
 
@@ -22,4 +20,40 @@ app.directive('fileDirective', function () {
     });
   };
 });
+*/
+
+/**
+ * USAGE:
+ * 1. Create the dropzoneConfig file in the AngularJS Controller.
+ */
+
+/*
+$scope.dropzoneConfig = {
+  'options': { // passed into the Dropzone constructor
+    'url': '/upload',
+    'paramName' : 'image', // The name that will be used to transfer the file
+    'maxFilesize': 3, // MB
+  },
+  'eventHandlers': {
+    'sending': function (file, xhr, formData) {
+      console.log('Sending!!!!');
+    },
+    'success': function (file, response) {
+      console.log('Success!!!!');
+    }
+  }
+};
+*/
+
+/**
+ * 2. Pass this Config file into the directive on the HTML Like this:
+ */
+
+/*
+<form
+action="/file-upload"
+class="dropzone"
+id="my-awesome-dropzone"
+uprm-file-uploader="dropzoneConfig">
+  </form>
 */
