04fdf4656e313b01de869339114dacc03e8847d3
hellhunt <blackdragon80@live.com>
2016-03-22 05:53:00 -0400

    Recruiter Registration Validation

--- app/scripts/controllers/recruiter-registration.js
+++ app/scripts/controllers/recruiter-registration.js
@@ -5,5 +5,15 @@ var app = angular.module('uprmcmsApp');
 app.controller('RecruiterRegistrationCtrl', function($scope, $state, localStorageService) {
 
   $scope.companyName = localStorageService.get('companyToRegister');
+  $scope.companyLocation = localStorageService.get('companyLocationToRegister');
+
+
+  $scope.registerRecruiter = function(isValid) {
+    // check to make sure the form is completely valid
+    if (isValid) {
+      $state.go('company');
+    }
+  };
+
 
 });
