1ea1465e4656f6b04e323e3c6517dc9389944421
Eduardo M. Breijo <eduardo.breijo@upr.edu>
2016-03-22 17:50:39 -0400

    Fixed flexslider to refresh everytime the landing page is loaded.

--- app/scripts/controllers/landingPageController.js
+++ app/scripts/controllers/landingPageController.js
@@ -22,10 +22,18 @@ app.controller('LandingPageCtrl', function($scope) {
     }
   };
 
+  // Once the view is loaded, parse the Facebook plugin and load the slider
   $scope.$on('$viewContentLoaded', function() {
     /* jshint ignore:start */
     FB.XFBML.parse(document.getElementById('promo-facebook-plugin'));
     /* jshint ignore:end */
+
+    // Slider
+    $(document).ready(function() {
+      $('.flexslider').flexslider({
+        animation: 'slide'
+      });
+    });
   });
 
 });
