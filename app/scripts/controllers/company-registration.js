594cc5f6cbeeb49ee6514118990321de943f8fb2
hellhunt <blackdragon80@live.com>
2016-03-26 00:04:44 -0400

    Guest View Update v1.0

--- app/scripts/controllers/company-registration.js
+++ app/scripts/controllers/company-registration.js
@@ -50,15 +50,30 @@ app.controller('CompanyRegistrationCtrl', function($scope, $state) {
   };
 
   $scope.addMajors = function() {
-    // check to make sure the form is completely valid
     angular.forEach($scope.majorList, function (item) {
       if (item.value === true && (contains(item.name, $scope.majorsSelected) === false)){
-        $scope.majorsSelected.push({name: majors[i].majorCode, value: false});
+        $scope.majorsSelected.push({name: item.name, value: false});
+        var index = $scope.majorList.indexOf(item);
+        if (index > -1) {
+          $scope.majorList.splice(index, 1);
+        }
       }
     });
   };
 
-  $scope.registerLocation = function(isValid) {
+  $scope.removeMajors = function() {
+    angular.forEach($scope.majorsSelected, function (item) {
+      if (item.value === true && (contains(item.name, $scope.majorList) === false)){
+        $scope.majorList.push({name: item.name, value: false});
+        var index = $scope.majorsSelected.indexOf(item);
+        if (index > -1) {
+          $scope.majorsSelected.splice(index, 1);
+        }
+      }
+    });
+  };
+
+  $scope.registerCompany = function(isValid) {
     // check to make sure the form is completely valid
     if (isValid) {
       $state.go('company');
