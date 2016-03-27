fe1d36cf51ca273a9abd4c481f71caff4f5b19d0
hellhunt <blackdragon80@live.com>
2016-03-27 04:19:28 -0400

    Modal for Editing Pt1

--- app/scripts/controllers/companies/companyProfile.js
+++ app/scripts/controllers/companies/companyProfile.js
@@ -6,44 +6,80 @@ var app = angular.module('uprmcmsApp');
 //$scope.aboutUs = aboutUs.aboutUsInfo;
 app.controller('CompanyCtrl', function($scope) {
 
-  //emails
-  //user1@ibm.com
-  //user34@foundation.org
-  //Passwords
-  //jwjefW@34
-  //9Tbfdh#ld
+  $scope.tempCompanyInfo = [];
 
 
-  $scope.submitForm = function() {
-    // check to make sure the form is completely valid
-    if (isValid) {
-
-      for (i = 0; i < $scope.users.length; i++) {
-        if (($scope.user.email == $scope.users[i].email) && ($scope.user.password == $scope.users[i].password)){
-          return true;
-        }
-        else {
-          return false;
-        }
-      }
-    }
-
-  };
-
-  $scope.users = [
+  var majors = [
     {
-      "email": "user1@ibm.com",
-      "password": "jwjefW@34",
-      //"isRoot": false,
-      //"adminAccountStatus": "active"
+      'majorCode': 'CCOM',
+      'nameEnglish': 'Computer Science',
+      'nameSpanish': 'Ciencias de Computos'
     },
     {
-      "email": "user34@foundation.org",
-      "password": "9Tbfdh#ld",
-      //"email": "placement@uprm.edu",
-      //"isRoot": true,
-      //"adminAccountStatus": "active"
+      'majorCode': 'ICOM',
+      'nameEnglish': 'Computer Engineering',
+      'nameSpanish': 'Ingenieria en Computadoras'
+    },
+    {
+      'majorCode': 'INSO',
+      'nameEnglish': 'Software Engineering',
+      'nameSpanish': 'Ingenieria de Software'
+    },
+    {
+      'majorCode': 'INME',
+      'nameEnglish': 'Mechanical Engineering',
+      'nameSpanish': 'Ingenieria Mecanica'
+    },
+    {
+      'majorCode': 'INEL',
+      'nameEnglish': 'Electrical Engineering',
+      'nameSpanish': 'Ingenieria Electrica'
     }
-  ]
+  ];
+
+  var companyInfo = {
+    name: "IBM",
+    websiteUrl: "http://www.ibm.com/us-en/",
+    logoPath: null,
+    companyDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat quis elit a vestibulum. Mauris leo sem, lacinia eu dapibus a, tempor eget metus. In vehicula maximus magna. Vestibulum pulvinar purus in tristique pellentesque. Sed porta imperdiet ultricies. Cras ac ipsum aliquam, condimentum risus non, euismod quam. Morbi posuere lobortis auctor. Aliquam massa eros, ultrices a viverra a, lacinia sed sem.",
+    companyStatus: "Active"
+  };
+
+  $scope.companyProfile = {
+    'generalInfo':[
+
+    ],
+    'interestedMajors':[
+      {
+
+      }
+    ],
+    'promotionalMaterial':[
+      {
+
+      }
+    ],
+    'recruiterList':[
+      {
+
+      }
+    ],
+    'requestsInfo':[
+      {
+
+      }
+    ]
+  };
+
+  $scope.companyProfile.generalInfo.push({name: companyInfo.name, websiteUrl: companyInfo.websiteUrl, logoPath: companyInfo.logoPath, companyDescription: companyInfo.companyDescription, companyStatus: companyInfo.companyStatus });
+
+  $scope.tempCompanyInfo.push({name: companyInfo.name, websiteUrl: companyInfo.websiteUrl, logoPath: companyInfo.logoPath, companyDescription: companyInfo.companyDescription, companyStatus: companyInfo.companyStatus });
+
+  $scope.updateCompanyDescription = function(isValid){
+    if (isValid){
+      $scope.companyProfile.generalInfo[0].websiteUrl = $scope.tempCompanyInfo[0].websiteUrl;
+      $scope.companyProfile.generalInfo[0].companyDescription = $scope.tempCompanyInfo[0].companyDescription;
+    }
+  };
 
 });
