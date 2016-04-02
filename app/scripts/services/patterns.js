c0ba7f42df8752472e4fa3e6c56e08dfcf310583
Eduardo M. Breijo <eduardo.breijo@upr.edu>
2016-04-02 16:50:23 -0400

    Implemented and integrated add/edit/remove staff for admins. Fixed the position field in ourStaff model to accept only characters between 2 and 63 of length.

--- app/scripts/services/patterns.js
+++ app/scripts/services/patterns.js
@@ -6,6 +6,9 @@ app.factory('Patterns', function() {
   return {
     user: {
       email: /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
+    },
+    ourStaff: {
+      name: /^([a-zA-ZñÑáéíóúÁÉÍÓÚ.][\s]?){2,63}$/
     }
   };
 });
