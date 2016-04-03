9edceef77d1af61fb345a4bfa8ad4a8c35e33d66
Jean <jean.mendez2@upr.edu>
2016-04-03 16:40:25 -0400

    Added pictures service and changed Landing Page HTML to use the json in this service to display the images.

---
+++ app/scripts/services/pictures.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var app = angular.module('uprmcmsApp');
+
+app.factory('Pictures', function() {
+  var obj = {
+    pictures: [
+      {
+        id: 1,
+        fileLabel: 'photo1',
+        filePath: '../../media/landing/slide-1.jpg'
+      },
+      {
+        id: 2,
+        fileLabel: 'photo2',
+        filePath: '../../media/landing/slide-2.jpg'
+      },
+      {
+        id: 3,
+        fileLabel: 'photo3',
+        filePath: '../../media/landing/slide-3.jpg'
+      }
+    ]
+  };
+
+  return obj;
+});
