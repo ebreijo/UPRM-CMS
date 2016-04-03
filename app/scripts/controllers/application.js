29068c4f9df4a91455d571e6bf02c48d899d2e5b
Eduardo M. Breijo <eduardo.breijo@upr.edu>
2016-04-03 13:53:53 -0400

    Added some more validations in the admin section for creating companies, majors, and editing job offers. Added more validation in job offers and companies models. Added the sign out functionality for admins navbar.

--- app/scripts/controllers/application.js
+++ app/scripts/controllers/application.js
@@ -2,10 +2,21 @@
 
 var app = angular.module('uprmcmsApp');
 
-app.controller('ApplicationCtrl', function ($scope, USER_ROLES) {
+app.controller('ApplicationCtrl', function ($scope, $state, USER_ROLES, Auth, AUTH_EVENTS) {
 
   // variables for use in child scopes.
   $scope.currentUser = null;
   $scope.userRoles = USER_ROLES;
+
+  // Logout function to use in child scopes
+  $scope.logout = Auth.logout;
+
+  $scope.$on(AUTH_EVENTS.loginSuccess, function(event, user) {
+    $scope.currentUser = user;
+  });
+
+  $scope.$on(AUTH_EVENTS.logoutSuccess, function() {
+    $scope.currentUser = null;
+  });
 
 });
