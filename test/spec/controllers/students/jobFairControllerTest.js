f8f6c6b752a3b51ad8222b9615d6301539cb8bca
Jean <jean.mendez2@upr.edu>
2016-04-05 17:16:03 -0400

    Added two test scripts and removed another one.

---
+++ test/spec/controllers/students/jobFairControllerTest.js
@@ -0,0 +1,33 @@
+'use strict';
+
+describe('Controller: Job Fair', function () {
+
+  // load the service's module
+  beforeEach(module('uprmcmsApp'));
+
+  /*
+  var $controller;
+  var scope;
+  var AboutUs;
+  var filter;
+  var _;
+  */
+
+  console.log('Controller: Job Fair');
+
+  // Call the controller and inject it with mock objects
+  /*
+  beforeEach(inject(function (_$controller_, $rootScope) {
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+    scope = $rootScope.$new();
+    $controller('AboutUsCtrl', {
+      $scope: scope,
+      AboutUs: AboutUs,
+      _: _,
+      $filter: filter
+    });
+  }));
+  */
+
+});
---
+++ test/spec/controllers/students/jobOffersControllerTest.js
@@ -0,0 +1,42 @@
+'use strict';
+
+describe('Controller: Job Offers', function () {
+
+  // load the service's module
+  beforeEach(module('uprmcmsApp'));
+
+  /*
+  var $controller;
+  var scope;
+  var AboutUs;
+  var filter;
+  var _;
+  var Patterns;
+
+  console.log('Controller: Job Offers');
+  */
+
+  // Get params for About Us controller
+  /*
+  beforeEach(inject(function ($injector) {
+    _ = $injector.get('_');
+  }));
+
+  // Call the controller and inject it with mock objects
+  beforeEach(inject(function (_$controller_, $rootScope) {
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+    scope = $rootScope.$new();
+
+    $controller('AboutUsCtrl', {
+      $scope: scope,
+      AboutUs: AboutUs,
+      _: _,
+      $filter: filter,
+      Patterns: Patterns
+    });
+
+  }));
+  */
+
+});
