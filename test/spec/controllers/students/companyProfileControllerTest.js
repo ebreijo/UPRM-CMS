8e9b19a93e2f7febf0aa5ebfd6ccad4ed50ff9bc
Jean <jean.mendez2@upr.edu>
2016-04-05 16:42:24 -0400

    Updated karma.conf.js to delete previous crypto references.

---
+++ test/spec/controllers/students/companyProfileControllerTest.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Controller: Company Profile', function () {
+
+  // load the service's module
+  beforeEach(module('uprmcmsApp'));
+
+  // Get params for About Us controller
+  /*
+  beforeEach(inject(function ($injector) {
+    AboutUs = $injector.get('AboutUs');
+    Patterns = $injector.get('Patterns');
+    _ = $injector.get('_');
+    filter = $injector.get('$filter');
+    q = $injector.get('$q');
+  }));
+  */
+
+});
