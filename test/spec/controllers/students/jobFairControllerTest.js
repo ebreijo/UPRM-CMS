82f65007d33a157e890d99d9402857ae67ad52f6
Jean <jean.mendez2@upr.edu>
2016-04-05 17:46:32 -0400

    Added first two tests for Job Offers Controller

--- test/spec/controllers/students/jobFairControllerTest.js
+++ test/spec/controllers/students/jobFairControllerTest.js
@@ -5,18 +5,17 @@ describe('Controller: Job Fair', function () {
   // load the service's module
   beforeEach(module('uprmcmsApp'));
 
-  /*
+
   var $controller;
   var scope;
   var AboutUs;
   var filter;
   var _;
-  */
 
   console.log('Controller: Job Fair');
 
   // Call the controller and inject it with mock objects
-  /*
+
   beforeEach(inject(function (_$controller_, $rootScope) {
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $controller = _$controller_;
@@ -28,6 +27,5 @@ describe('Controller: Job Fair', function () {
       $filter: filter
     });
   }));
-  */
 
 });
--- test/spec/controllers/students/jobOffersControllerTest.js
+++ test/spec/controllers/students/jobOffersControllerTest.js
@@ -5,38 +5,47 @@ describe('Controller: Job Offers', function () {
   // load the service's module
   beforeEach(module('uprmcmsApp'));
 
-  /*
   var $controller;
+  var JobOffers;
   var scope;
-  var AboutUs;
-  var filter;
-  var _;
-  var Patterns;
 
-  console.log('Controller: Job Offers');
-  */
-
-  // Get params for About Us controller
-  /*
+  // Inject Custom Services:
   beforeEach(inject(function ($injector) {
-    _ = $injector.get('_');
+    JobOffers = $injector.get('JobOffers');
   }));
 
-  // Call the controller and inject it with mock objects
+  // Inject AngularJS Services:
   beforeEach(inject(function (_$controller_, $rootScope) {
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $controller = _$controller_;
     scope = $rootScope.$new();
 
-    $controller('AboutUsCtrl', {
+    $controller('jobOffersCtrl', {
       $scope: scope,
-      AboutUs: AboutUs,
-      _: _,
-      $filter: filter,
-      Patterns: Patterns
+      JobOffers: JobOffers
+    });
+  }));
+
+  describe('scope.lookingForFilter function', function () {
+    it('should return true if all elements of check mark are false', function () {
+      scope.checkBox = {
+        internship: false,
+        coop: false,
+        partTime: false,
+        fullTime: false
+      };
+      expect(scope.lookingForFilter('Internship')).toEqual(true);
     });
 
-  }));
-  */
+    it('should return true internship is checked and internship is passed to filter (regardless of other values)', function () {
+      scope.checkBox = {
+        internship: true,
+        coop: false,
+        partTime: false,
+        fullTime: true
+      };
+      expect(scope.lookingForFilter('Internship')).toEqual(true);
+    });
+  });
 
 });
