0e17e819ca9e0b3fa5e4b211ddbcb265d0018ba1
Jean <jean.mendez2@upr.edu>
2016-04-05 17:54:18 -0400

    Completed Job Offers Test.

--- test/spec/controllers/students/jobOffersControllerTest.js
+++ test/spec/controllers/students/jobOffersControllerTest.js
@@ -27,6 +27,10 @@ describe('Controller: Job Offers', function () {
   }));
 
   describe('scope.lookingForFilter function', function () {
+
+    /**
+     * All Options Checked
+     */
     it('should return true if all elements of check mark are false', function () {
       scope.checkBox = {
         internship: false,
@@ -37,7 +41,10 @@ describe('Controller: Job Offers', function () {
       expect(scope.lookingForFilter('Internship')).toEqual(true);
     });
 
-    it('should return true internship is checked and internship is passed to filter (regardless of other values)', function () {
+    /**
+     * Internship
+     */
+    it('should return true if internship is checked and internship is passed to filter (regardless of other values)', function () {
       scope.checkBox = {
         internship: true,
         coop: false,
@@ -46,6 +53,89 @@ describe('Controller: Job Offers', function () {
       };
       expect(scope.lookingForFilter('Internship')).toEqual(true);
     });
+
+
+    it('should return false if internship is not checked and internship is passed to filter (regardless of other values)', function () {
+      scope.checkBox = {
+        internship: false,
+        coop: false,
+        partTime: false,
+        fullTime: true
+      };
+      expect(scope.lookingForFilter('Internship')).toEqual(false);
+    });
+
+    /**
+     * Co-Op
+     */
+    it('should return true if coop is checked and coop is passed to filter (regardless of other values)', function () {
+      scope.checkBox = {
+        internship: true,
+        coop: true,
+        partTime: true,
+        fullTime: true
+      };
+      expect(scope.lookingForFilter('CO-OP')).toEqual(true);
+    });
+
+
+    it('should return false if coop is not checked and coop is passed to filter (regardless of other values)', function () {
+      scope.checkBox = {
+        internship: false,
+        coop: false,
+        partTime: false,
+        fullTime: true
+      };
+      expect(scope.lookingForFilter('CO-OP')).toEqual(false);
+    });
+
+    /**
+     * Part-Time
+     */
+    it('should return true if part time is checked and part time is passed to filter (regardless of other values)', function () {
+      scope.checkBox = {
+        internship: true,
+        coop: false,
+        partTime: true,
+        fullTime: true
+      };
+      expect(scope.lookingForFilter('Part-Time')).toEqual(true);
+    });
+
+
+    it('should return false if part time is not checked and part time is passed to filter (regardless of other values)', function () {
+      scope.checkBox = {
+        internship: false,
+        coop: false,
+        partTime: false,
+        fullTime: true
+      };
+      expect(scope.lookingForFilter('Part-Time')).toEqual(false);
+    });
+
+    /**
+     * Full-Time
+     */
+    it('should return true if full time is checked and full time is passed to filter (regardless of other values)', function () {
+      scope.checkBox = {
+        internship: true,
+        coop: true,
+        partTime: false,
+        fullTime: true
+      };
+      expect(scope.lookingForFilter('Full-Time')).toEqual(true);
+    });
+
+    it('should return false if full time is not checked and full time is passed to filter (regardless of other values)', function () {
+      scope.checkBox = {
+        internship: true,
+        coop: false,
+        partTime: false,
+        fullTime: false
+      };
+      expect(scope.lookingForFilter('Full-Time')).toEqual(false);
+    });
+
   });
 
 });
