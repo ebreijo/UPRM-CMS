b784e5d0e97b7a972a65b216e4178d41cc647e9f
hellhunt <blackdragon80@live.com>
2016-04-06 00:25:03 -0400

    Company Registration Test Final

--- test/spec/controllers/companyRegistration.js
+++ test/spec/controllers/companyRegistration.js
@@ -216,4 +216,180 @@ describe('Controller: Company Registration', function () {
 
   });
 
+  describe('scope.removeMajors function', function () {
+
+    it('It should add majors selected from the list of interested majors to the list of all majors available. Case 1: List of all majors is empty', function () {
+
+      scope.majorList = [
+
+      ];
+
+      scope.majorsSelected = [
+        {
+          'name': 'CCOM',
+          'value': true
+        },
+        {
+          'name': 'ICOM',
+          'value': true
+        },
+        {
+          'name': 'INSO',
+          'value': true
+        },
+        {
+          'name': 'INME',
+          'value': true
+        },
+        {
+          'name': 'INEL',
+          'value': true
+        }
+      ];
+
+      var resultingList = [
+        {
+          'name': 'CCOM',
+          'value': false
+        },
+        {
+          'name': 'ICOM',
+          'value': false
+        },
+        {
+          'name': 'INSO',
+          'value': false
+        },
+        {
+          'name': 'INME',
+          'value': false
+        },
+        {
+          'name': 'INEL',
+          'value': false
+        }
+      ];
+
+      scope.removeMajors();
+
+      expect(angular.equals(scope.majorList, resultingList)).toBeTruthy();
+
+    });
+
+
+    it('It should add majors selected from all majors available to the list of interested majors of a company. Case 2: List of interested majors is empty', function () {
+
+      scope.majorList = [
+        {
+          'name': 'CCOM',
+          'value': false
+        },
+        {
+          'name': 'ICOM',
+          'value': false
+        },
+        {
+          'name': 'INSO',
+          'value': false
+        },
+        {
+          'name': 'INME',
+          'value': false
+        },
+        {
+          'name': 'INEL',
+          'value': false
+        }
+      ];
+
+      scope.majorsSelected = [
+
+      ];
+
+      var resultingList = [
+        {
+          'name': 'CCOM',
+          'value': false
+        },
+        {
+          'name': 'ICOM',
+          'value': false
+        },
+        {
+          'name': 'INSO',
+          'value': false
+        },
+        {
+          'name': 'INME',
+          'value': false
+        },
+        {
+          'name': 'INEL',
+          'value': false
+        }
+      ];
+
+      scope.removeMajors();
+
+      expect(angular.equals(scope.majorList, resultingList)).toBeTruthy();
+
+    });
+
+    it('It should add majors selected from all majors available to the list of interested majors of a company. Case 3: Some majors are selected from the list of interested majors', function () {
+
+      scope.majorList = [
+        {
+          'name': 'INSO',
+          'value': false
+        },
+        {
+          'name': 'INME',
+          'value': false
+        }
+      ];
+
+      scope.majorsSelected = [
+        {
+          'name': 'CCOM',
+          'value': true
+        },
+        {
+          'name': 'ICOM',
+          'value': false
+        },
+        {
+          'name': 'INEL',
+          'value': true
+        }
+      ];
+
+      var resultingList = [
+        {
+          'name': 'INSO',
+          'value': false
+        },
+        {
+          'name': 'INME',
+          'value': false
+        },
+
+        {
+          'name': 'CCOM',
+          'value': false
+        },
+
+        {
+          'name': 'INEL',
+          'value': false
+        }
+      ];
+
+      scope.removeMajors();
+
+      expect(angular.equals(scope.majorList, resultingList)).toBeTruthy();
+
+    });
+
+  });
+
 });
