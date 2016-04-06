4e0c3397b8058716bcf20e2977fd6cc00a33d850
hellhunt <blackdragon80@live.com>
2016-04-05 22:12:32 -0400

    Recruiter Registration Test

--- test/spec/controllers/recruiterRegistration.js
+++ test/spec/controllers/recruiterRegistration.js
@@ -1,3 +1,53 @@
-/**
- * Created by artic_hunt on 4/5/16.
- */
+'use strict';
+
+describe('Controller: Location Search', function () {
+
+  // load the service's module
+  beforeEach(module('uprmcmsApp'));
+
+  var $controller;
+  var recruiterRegistration;
+  var scope;
+
+  var $state;
+  var localStorageService;
+
+  // Inject Custom Services:
+
+  beforeEach(inject(function ($injector) {
+    localStorageService = $injector.get('localStorageService');
+  }));
+
+  // Inject AngularJS Services:
+  beforeEach(inject(function (_$controller_, $rootScope, _$state_) {
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+    scope = $rootScope.$new();
+    $state = _$state_;
+
+    $controller('RecruiterRegistrationCtrl', {
+      $scope: scope,
+      recruiterRegistration: recruiterRegistration,
+      $state: $state
+    });
+
+  }));
+
+  describe('scope.registerRecruiter function', function () {
+
+    it('It should register the recruiter and go to the recruiter company profile page if inputs in form are valid', function () {
+
+      expect(scope.registerRecruiter(true)).toEqual(true);
+
+    });
+
+    it('It should stay on the recruiterRegistration web page if inputs in form are not valid', function () {
+
+      expect(scope.registerRecruiter(false)).toEqual(false);
+
+    });
+
+  });
+
+
+});
