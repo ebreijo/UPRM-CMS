07a008ab3388f9603aaa23f9765d17c3710cfa57
Jean <jean.mendez2@upr.edu>
2016-04-20 17:29:23 -0400

    Fixed Home Page Picture upload route on server and added upload button on Change Carousel View.

--- app/scripts/controllers/administrators/adminChangeCarousel.js
+++ app/scripts/controllers/administrators/adminChangeCarousel.js
@@ -4,7 +4,34 @@ var app = angular.module('uprmcmsApp');
 
 app.controller('AdminChangeCarousel', function($scope, Pictures) {
 
-  $scope.lol = 'lol';
   $scope.pictures = Pictures.pictures;
+
+  /* jshint ignore:start */
+  $scope.uploadPictureConfig = {
+    'options': { // passed into the Dropzone constructor
+      'url': '/api/pictures',
+      'paramName': 'image',     // The name that will be used to transfer the file
+      'maxFilesize': 2, // in MBs
+      'maxFiles': 1,
+      'acceptedFiles': 'image/jpeg,image/png',
+      'createImageThumbnails': false
+    },
+    'eventHandlers': {
+      'sending': function (file, xhr, formData) {
+        console.log('Sending!!!!');
+      },
+      'success': function (file, response) {
+        console.log('Success!!!!');
+        this.removeAllFiles();
+        //Companies.updateCompanyGeneralInformation({"logoPath": response.filePath}, $scope.getCurrentUser().companyName);
+        //$scope.companyProfile.generalInfo[0].logoPath = response.filePath;
+      },
+      'error': function(file, response) {
+        this.removeAllFiles();
+        alert('ERROR: File Too Large!');
+      }
+    }
+  };
+  /* jshint ignore:end */
 
 });
