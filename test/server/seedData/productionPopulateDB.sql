87baf35f61782718ceaed4f177ef95aebd74d0d4
Eduardo M. Breijo <eduardo.breijo@upr.edu>
2016-04-28 11:24:14 -0400

    Added sql script for production

---
+++ test/server/seedData/productionPopulateDB.sql
@@ -0,0 +1,156 @@
+INSERT INTO about_us (vision, mission_desc, policies_desc, requirements_desc)
+VALUES ('Serve as liaison between students and businesses while providing the best and most effective service to all.',
+'Provide students the necessary tools that will help achieve an effective job search, while maintaining lines of communication with businesses and the College community.',
+'The following, apply to all students, seniors, graduate and alumni that request our services:',
+'The following, apply to all students, seniors, graduate and alumni that request our services:');
+
+INSERT INTO administrator_access (email, is_root, admin_account_status)
+VALUES ('placement@uprm.edu', 1, 'pending');
+
+INSERT INTO company_services (service)
+VALUES ('Announce any job offers (part time, summer and permanent)');
+
+INSERT INTO company_services (service)
+VALUES ('Collect and send resumes of students and alumni');
+
+INSERT INTO company_services (service)
+VALUES ('Coordinate on-campus interviews');
+
+INSERT INTO company_services (service)
+VALUES ('Organize the Annual Job Fair');
+
+INSERT INTO company_services (service)
+VALUES ('Organize information sessions');
+
+INSERT INTO company_services (service)
+VALUES ('Coordinate meetings with faculty and student organizations');
+
+INSERT INTO company_services (service)
+VALUES ('We refer resumes of recent graduates with minimum experience.');
+
+INSERT INTO home_page_photos (file_label, file_path)
+VALUES ('photo1', 'media/landing/628ff789a945c81bc53273e5fafdfb65.jpeg');
+
+INSERT INTO home_page_photos (file_label, file_path)
+VALUES ('photo2', 'media/landing/99842a6ebf72dacd7585cf010c0e2149.jpeg');
+
+INSERT INTO home_page_photos (file_label, file_path)
+VALUES ('photo3', 'media/landing/9547ade0666c15d4e1f5b6f9fac6f5cf.jpeg');
+
+INSERT INTO home_page_photos (file_label, file_path)
+VALUES ('photo4', 'media/landing/36aedd4cf64b499de8412354af1635ef.jpeg');
+
+INSERT INTO home_page_photos (file_label, file_path)
+VALUES ('photo5', 'media/landing/c052d690e3698b00a2e6b887a607b684.jpeg');
+
+INSERT INTO job_fair_dates (header_en, location_en, date_en, time, header_es, location_es, date_es, resume_deadline_date, show_resume_deadline_date)
+VALUES ('28th Annual Job Fair', 'Rafael A. Mangual Campus Coliseum', 'Friday, September 30, 2016', '8:30am - 2:30pm', '28va Feria de Empleo', 'Coliseo Rafaeo A. Mangual', 'viernes, 30 de septiembre de 2016', '2016-09-30 04:00:00', '1');
+
+INSERT INTO major (major_code, name_english, name_spanish)
+VALUES ('ICOM', 'Computer Engineering', 'Ingenieria en Computadoras');
+
+INSERT INTO major (major_code, name_english, name_spanish)
+VALUES ('CCOM', 'Computer Science', 'Ciencias de Computos');
+
+INSERT INTO major (major_code, name_english, name_spanish)
+VALUES ('INSO', 'Software Engineering', 'Ingenieria de Software');
+
+INSERT INTO major (major_code, name_english, name_spanish)
+VALUES ('INME', 'Mechanical Engineering', 'Ingenieria Mecanica');
+
+INSERT INTO major (major_code, name_english, name_spanish)
+VALUES ('ININ', 'Industrial Engineering', 'Ingenieria Industrial');
+
+INSERT INTO major (major_code, name_english, name_spanish)
+VALUES ('INGL', 'English', 'Ingles');
+
+INSERT INTO major (major_code, name_english, name_spanish)
+VALUES ('ESPA', 'Spanish', 'Español');
+
+INSERT INTO major (major_code, name_english, name_spanish)
+VALUES ('MATE', 'Mathematics', 'Matematicas');
+
+INSERT INTO our_staff (name, position)
+VALUES ('Sra. Nancy Nieves Arán', 'Director');
+
+INSERT INTO our_staff (name, position)
+VALUES ('Srta. Margarita Carlo Cuebas', 'Employment Interviewer');
+
+INSERT INTO our_staff (name, position)
+VALUES ('Sr. William Pacheco', 'Employment Interviewer');
+
+INSERT INTO our_staff (name, position)
+VALUES ('Sra. Eva E. Troche Morales', 'Administrative Secretary');
+
+INSERT INTO policies (policy)
+VALUES ('Register at the Placement Office with any member of the staff.');
+
+INSERT INTO policies (policy)
+VALUES ('Clear through any staff member, if you miss an interview. Any student who fails in this aspect for a second time will not be allowed future interviews. Remember your actions will reflect on your peers.');
+
+INSERT INTO policies (policy)
+VALUES ('Every student must sign up for an interview on their spare time. The Placement Office will not provide excuse letters for missing classes on account of an interview.');
+
+INSERT INTO policies (policy)
+VALUES ('There is no limit to the number of interviews a student can have. However, once a student has accepted a job offer, he or she must stop interviewing.');
+
+INSERT INTO policies (policy)
+VALUES ('Students that accept a job offer should notify the Placement Office.');
+
+INSERT INTO requirements (requirement)
+VALUES ('Five or more copies of your resume (preferably in English).');
+
+INSERT INTO requirements (requirement)
+VALUES ('Copy of your course program.');
+
+INSERT INTO requirements (requirement)
+VALUES ('Transcript (preferably in English).');
+
+INSERT INTO requirements (requirement)
+VALUES ('Fill out student evaluation form');
+
+INSERT INTO requirements (requirement)
+VALUES ('2x2 photograph (optional).');
+
+INSERT INTO requirements (requirement)
+VALUES ('Every student is responsible for maintaining his/her file updated and with enough copies at the Placement Office.');
+
+INSERT INTO student_services (service)
+VALUES ('Register students and alumni, creating records for our files.');
+
+INSERT INTO student_services (service)
+VALUES ('Assist students and alumni explore the job market.');
+
+INSERT INTO student_services (service)
+VALUES ('Help with the correction of resumes.');
+
+INSERT INTO student_services (service)
+VALUES ('Offer talks to groups of students on resume writing, interviews, job search, etc.');
+
+INSERT INTO student_services (service)
+VALUES ('Coordinate on campus interviews.');
+
+INSERT INTO student_services (service)
+VALUES ('Refer resumes to companies and or agencies.');
+
+INSERT INTO student_services (service)
+VALUES ('Announce job opportunities (part-time, summer and permanent).');
+
+INSERT INTO student_services (service)
+VALUES ('Keep a list of companies and agencies with their addresses.');
+
+INSERT INTO student_services (service)
+VALUES ('Coordinate meetings between student organizations and companies.');
+
+INSERT INTO student_services (service)
+VALUES ('Prepare salary statistics.');
+
+INSERT INTO student_services (service)
+VALUES ('Prepare annual employment statistics.');
+
+INSERT INTO student_services (service)
+VALUES ('Organize Annual Job Fair.');
+
+INSERT INTO student_services (service)
+VALUES ('Work with student organizations.');
+
