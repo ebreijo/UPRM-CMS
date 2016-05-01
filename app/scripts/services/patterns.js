2594493ce34b66282c132c13fba4d38c2c55ef15
Eduardo M. Breijo <eduardo.breijo@upr.edu>
2016-05-01 12:54:30 -0400

    Added some characters to some regex to the admin company profile. Fixed some error messages.

--- app/scripts/services/patterns.js
+++ app/scripts/services/patterns.js
@@ -6,7 +6,7 @@ app.factory('Patterns', function() {
   return {
     user: {
       email: /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i,
-      firstName: /^[a-zA-ZñÑáéíóúÁÉÍÓÚ]{2,45}$/,
+      firstName: /^([a-zA-ZñÑáéíóúÁÉÍÓÚ][\s]?){2,45}$/,
       lastName: /^([a-zA-ZñÑáéíóúÁÉÍÓÚ][\s]?){2,45}$/,
       phoneNumber: /^\+?([1]{1})?[-]?([0-9]{3})\)?[-]([0-9]{3})[-]([0-9]{4})$/
     },
@@ -16,7 +16,7 @@ app.factory('Patterns', function() {
     jobFairManagement: {
       companyAdditionalInformation: /^[-a-z0-9 =¿?¡!/:._$,;*&@#%+'-()ñÑáéíóúÁÉÍÓÚ]+(\.)?$/i,
       minGpa: /^([0-3]\.\d\d|4\.00)$/i,
-      applicationWebsiteUrl: /^[-a-z0-9 =?/:._$,;*&@#%+'-()ñÑáéíóúÁÉÍÓÚ]+(\.)?$/i
+      applicationWebsiteUrl: /^[-a-z0-9 =¿?¡!/:._$,;*&@#%+'-()ñÑáéíóúÁÉÍÓÚ]+(\.)?$/i
     }
   };
 });
